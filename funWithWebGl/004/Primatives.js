275536873ab1c165edcdab69498c32ff8a9c825d
Carmen Cincotti <cincotti@tuta.io>
2020-11-15 13:54:24 -0500

    grid

--- funWithWebGl/004/Primatives.js
+++ funWithWebGl/004/Primatives.js
@@ -2,7 +2,40 @@ const Primatives = {}
 
 Primatives.GridAxis = class {
     static createMesh(gl) {
-        const verts = [ 0, 0.5, 0, 0,  0, -0.5, 0, 1 ]
+        // const verts = [ 0, 0.5, 0, 0,  0, -0.5, 0, 1 ]
+        //Dynamiclly create a grid
+		var verts = [],
+        size = 1.8,			// W/H of the outer box of the grid, from origin we can only go 1 unit in each direction, so from left to right is 2 units max
+        div = 10.0,			// How to divide up the grid
+        step = size / div,	// Steps between each line, just a number we increment by for each line in the grid.
+        half = size / 2;	// From origin the starting position is half the size.
+
+    var p;	//Temp variable for position value.
+    for(var i=0; i <= div; i++){
+        //Vertical line
+        p = -half + (i * step);
+        verts.push(p);		//x1
+        verts.push(half);	//y1
+        verts.push(0);		//z1
+        verts.push(0);		//c2
+
+        verts.push(p);		//x2
+        verts.push(-half);	//y2
+        verts.push(0);		//z2
+        verts.push(1);		//c2
+
+        //Horizontal line
+        p = half - (i * step);
+        verts.push(-half);	//x1
+        verts.push(p);		//y1
+        verts.push(0);		//z1
+        verts.push(0);		//c1
+
+        verts.push(half);	//x2
+        verts.push(p);		//y2
+        verts.push(0);		//z2
+        verts.push(1);		//c2
+    }
 
         const attrColorLoc = 4; // Same that we set up in shader. Define here so we can use it.
         const mesh = {
