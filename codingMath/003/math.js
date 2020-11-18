e17775a2cc316d63030f424717cad4d296228c8b
Carmen Cincotti <cincotti@tuta.io>
2020-11-18 13:56:19 -0500

    cosines and tangents

---
+++ codingMath/003/math.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+    const   canvas = document.getElementById("canvas"),
+            context = canvas.getContext('2d'),
+            width = canvas.width = window.innerWidth,
+            height = canvas.height = window.innerHeight
+
+    // Animating an Object
+
+    // Center on canvas
+    const centerY = height * 0.5
+    const centerX = width * 0.5
+
+    // How far to move the object on canvas
+    const offset = height * 0.4 
+
+    let baseRadius = 100
+    let speed = 0.1
+    let angle = 0
+
+    render()
+
+    function render() {
+        let y = centerY + Math.sin(angle) * offset
+
+        context.clearRect(0,0,width,height)
+        context.beginPath();
+        context.arc(centerX, y, 50, 0, Math.PI * 2, false);
+        context.fillStyle = "red"
+        context.fill()
+
+        angle += speed
+        requestAnimationFrame(render)
+    }
+}
