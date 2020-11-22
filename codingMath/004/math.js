c4937fca74355c6dc108cea50af6fdd6b2d99a28
Carmen Cincotti <cincotti@tuta.io>
2020-11-22 06:44:08 -0500

    math - 004 get circle going

--- codingMath/004/math.js
+++ codingMath/004/math.js
@@ -4,11 +4,33 @@ window.onload = function() {
     width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight
   
+  // Set center of circle.
+  // Remember (0,0) is top left of viewport.
+  const centerX = width / 2
+  const centerY = height / 2
+
+  const radius = 200
+  const speed = .01
+
+  let angle = 0, x, y
   
   render()
   
   function render() {
     context.clearRect(0,0,width,height)
+
+    // Need to bias by our center coord
+
+    // x = rcos(ø)
+    x = centerX + radius * Math.cos(angle)
+    // y = rsin(ø)
+    y = centerY + radius * Math.sin(angle)
+
+    context.beginPath();
+    context.arc(x, y, 10, 0, Math.PI * 2, false)
+    context.fill()
+
+    angle += speed
 
     requestAnimationFrame(render)
   }
