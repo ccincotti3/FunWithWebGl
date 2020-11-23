3700517e5da111f50b8dd318776e9438069f2b6d
Carmen Cincotti <cincotti@tuta.io>
2020-11-23 04:50:58 -0500

    Good cube render

--- funWithWebGl/009/Primatives.js
+++ funWithWebGl/009/Primatives.js
@@ -34,9 +34,9 @@ Primatives.MultiQuad = class {
       aUV = [ ], //0,0, 0,1, 1,1, 1,0 
       aVert = [];		
 
-    for(var i=0; i < 10; i++){
+    for(let i=0; i < 10; i++){
       //Calculate a random size, y rotation and position for the quad
-      var size = 0.2 + (0.8* Math.random()),		//Random Quad Size in the range of 0.2 - 1.0
+      const size = 0.2 + (0.8* Math.random()),		//Random Quad Size in the range of 0.2 - 1.0
         half = size * 0.5,						//Half of size, this is the radius for rotation
         angle = Math.PI * 2 * Math.random(),	//Random angle between 0 - 360 degrees in radians
         dx = half * Math.cos(angle),			//Calc the x distance, used as an offset for the random position
@@ -96,19 +96,84 @@ Primatives.CubeBad = class {
   }
 }
 
+Primatives.Cube = class {
+  static createModel(gl){ return new Model(Primatives.Cube.createMesh(gl,1,1,1,0,0,0)); }
+  static createMesh(gl,width,height,depth,x,y,z){
+    const w = width*0.5, h = height*0.5, d = depth*0.5;
+    const x0 = x-w, x1 = x+w, y0 = y-h, y1 = y+h, z0 = z-d, z1 = z+d;
+
+    //Starting bottom left corner, then working counter clockwise to create the front face.
+    //Backface is the first face but in reverse (3,2,1,0)
+    //keep each quad face built the same way to make index and uv easier to assign
+    const aVert = [
+      x0, y1, z1, 0,	//0 Front
+      x0, y0, z1, 0,	//1
+      x1, y0, z1, 0,	//2
+      x1, y1, z1, 0,	//3 
+
+      x1, y1, z0, 1,	//4 Back
+      x1, y0, z0, 1,	//5
+      x0, y0, z0, 1,	//6
+      x0, y1, z0, 1,	//7 
+
+      x0, y1, z0, 2,	//7 Left
+      x0, y0, z0, 2,	//6
+      x0, y0, z1, 2,	//1
+      x0, y1, z1, 2,	//0
+
+      x0, y0, z1, 3,	//1 Bottom
+      x0, y0, z0, 3,	//6
+      x1, y0, z0, 3,	//5
+      x1, y0, z1, 3,	//2
+
+      x1, y1, z1, 4,	//3 Right
+      x1, y0, z1, 4,	//2 
+      x1, y0, z0, 4,	//5
+      x1, y1, z0, 4,	//4
+
+      x0, y1, z0, 5,	//7 Top
+      x0, y1, z1, 5,	//0
+      x1, y1, z1, 5,	//3
+      x1, y1, z0, 5	//4
+    ];
+
+    //Build the index of each quad [0,1,2, 2,3,0]
+    const aIndex = [];
+    for(let i=0; i < aVert.length / 4; i+=2) aIndex.push(i, i+1, (Math.floor(i/4)*4)+((i+2)%4));
+
+    //Build UV data for each vertex
+    const aUV = [];
+    for(let i=0; i < 6; i++) aUV.push(0,0,	0,1,  1,1,  1,0);
+
+    //Build Normal data for each vertex
+    const aNorm = [
+			 0, 0, 1,	 0, 0, 1,	 0, 0, 1,	 0, 0, 1,		//Front
+			 0, 0,-1,	 0, 0,-1,	 0, 0,-1,	 0, 0,-1,		//Back
+      -1, 0, 0,	-1, 0, 0,	-1, 0,0 ,	-1, 0, 0,		//Left
+			 0,-1, 0,	 0,-1, 0,	 0,-1, 0,	 0,-1, 0,		//Bottom
+			 1, 0, 0,	 1, 0, 0,	 1, 0, 0,	 1, 0, 0,		//Right
+			 0, 1, 0,	 0, 1, 0,	 0, 1, 0,	 0, 1, 0		//Top
+    ]
+
+    const mesh = gl.fCreateMeshVAO("Cube",aIndex,aVert,aNorm,aUV,4);
+    mesh.noCulling = true;	//TODO Only setting this true to view animations better.
+    return mesh;
+  }
+}
+
 Primatives.GridAxis = class {
   static createModel(gl, inclAxis) { return new Model(Primatives.GridAxis.createMesh(gl, inclAxis)) }
   static createMesh(gl, inclAxis) {
     // const verts = [ 0, 0.5, 0, 0,  0, -0.5, 0, 1 ]
     //Dynamiclly create a grid
-    var verts = [],
+    const verts = [],
       size = 1.8,			// W/H of the outer box of the grid, from origin we can only go 1 unit in each direction, so from left to right is 2 units max
       div = 10.0,			// How to divide up the grid
       step = size / div,	// Steps between each line, just a number we increment by for each line in the grid.
       half = size / 2;	// From origin the starting position is half the size.
 
-    var p;	//Temp variable for position value.
-    for(var i=0; i <= div; i++){
+    let p;	//Temp variable for position value.
+    for(let i=0; i <= div; i++){
       //Vertical line
       p = -half + (i * step);
       verts.push(p);		//x1
